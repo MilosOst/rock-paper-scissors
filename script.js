e5db54c28a93b64b5f091ce849c248e0883cc080
Milos <ostojicmilos10@gmail.com>
2022-05-22 09:32:54 -0400

    Basic functionality complete

--- script.js
+++ script.js
@@ -1,72 +1,98 @@
+let userLives = 5;
+let cpuLives = 5;
+
+
 function computerPlay() {
             const choices = ['Rock', 'Paper', 'Scissors'];
             let selection = Math.ceil(Math.random() * 3) - 1;
             return choices[selection];
         }
 
-        function playRound(playerSelection, computerSelection) {
-            playerSelection = playerSelection.toLowerCase();
+function playRound(playerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerPlay()
 
-            // Immediately check for tie
-            if (playerSelection === computerSelection.toLowerCase()) {
-                return 'Tie';
-            }
-            
-            if (playerSelection === 'rock') {
-                if (computerSelection === 'Paper') {
-                    return 'You Lose! Paper beats Rock';
-                }
-                else {
-                    return 'You Win! Rock beats Scissors';
-                }
-            }
-            else if (playerSelection === 'paper') {
-                if (computerSelection === 'Rock') {
-                    return 'You Win! Paper beats Rock';
-                }
-                else {
-                    return 'You Lose! Paper loses to Scissors';
-                }
-            }
-            // Only remaining option is playerSelection === 'scissors'
-            else {
-                if (computerSelection === 'Rock') {
-                    return 'You Lose! Rock beats Scissors';
-                }
-                else if (computerSelection === 'Paper') {
-                    return 'You Win! Scissors beats Paper';
-                }
-                else {
-                    return 'Tie! Scissors ties with Scissors';
-                }
-            }
+    // Immediately check for tie
+    if (playerSelection === computerSelection.toLowerCase()) {
+        return 'Tie';
+    }
+    
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'Paper') {
+            userLives--;
+            return 'You Lost! Paper beats Rock';
         }
-
-        function game() {
-            let userScore = 0;
-            let cpuScore = 0;
-
-            for (let i = 0; i < 5; i++) {
-                let userChoice = prompt('Choose one of: Rock, Paper, Scissors');
-                let result = playRound(userChoice, computerPlay());
-                console.log(result);
-
-                if (result.includes('You Win!')) {
-                    userScore++;
-                }
-                else if (result.includes('You Lose!')) {
-                    cpuScore++;
-                }
-            }
-
-            // Calculate final results
-            if (userScore > cpuScore) {
-                console.log(`You won ${userScore} to ${cpuScore}`);
-            }
-            else if (userScore === cpuScore) {
-                console.log(`You tied ${userScore} to ${userScore}`);
-            }
-            else {
-                console.log(`You lost ${cpuScore} to ${userScore}`);
-            }
+        else {
+            cpuLives--;
+            return 'You Won! Rock beats Scissors';
         }
+    }
+    else if (playerSelection === 'paper') {
+        if (computerSelection === 'Rock') {
+            cpuLives--;
+            return 'You Won! Paper beats Rock';
+        }
+        else {
+            userLives--;
+            return 'You Lost! Paper Losts to Scissors';
+        }
+    }
+    // Only remaining option is playerSelection === 'scissors'
+    else {
+        if (computerSelection === 'Rock') {
+            userLives--;
+            return 'You Lost! Rock beats Scissors';
+        }
+        else if (computerSelection === 'Paper') {
+            cpuLives--;
+            return 'You Won! Scissors beats Paper';
+        }
+        else {
+            return 'Tie! Scissors ties with Scissors';
+        }
+    }
+}
+
+function game() {
+
+
+    // Calculate final results
+    if (userScore > cpuScore) {
+        console.log(`You won ${userScore} to ${cpuScore}`);
+    }
+    else if (userScore === cpuScore) {
+        console.log(`You tied ${userScore} to ${userScore}`);
+    }
+    else {
+        console.log(`You lost ${cpuScore} to ${userScore}`);
+    }
+}
+
+function handleMove(button) {
+    let playerMove = button.id.replace('Btn', ''); // Converts move to one of ('rock', 'paper', 'scissors)
+    let result = playRound(playerMove);
+
+    const resultElement = document.querySelector('.results')
+    resultElement.textContent = result;
+
+    if (result.includes('You Won')) resultElement.style.color = 'rgba(0, 175, 62, 1)';
+    else if (result.includes('You Lost!')) resultElement.style.color = 'red';
+    else resultElement.style.color = 'white';
+
+    if (!userLives || !cpuLives) {
+        console.log('Game Over')
+        resetGame();
+    };
+}
+
+
+function resetGame() {
+    userLives = 5;
+    cpuLives = 5;
+    const resultElement = document.querySelector('.results')
+    resultElement.textContent = '';
+}
+
+
+playerButtons = [...document.querySelectorAll('.choices-box .btn')];
+playerButtons.forEach((button) => button.addEventListener('click', () => handleMove(button)));
